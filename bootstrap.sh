b1a7f78fd748f83b8b5bfe9a4d40fe5cf774d18f
Ty Foster <tyfoster@outlook.com>
2025-03-26 23:00:19 -0700

    Bootstrap test cluster

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,9 @@ set -e
 
 extra_args=""
 vault_password_file="~/vault"
+playbook="bootstrap"
 
-while getopts ":v:l:t:" opt; do
+while getopts ":v:l:t:p:" opt; do
   case ${opt} in
     v )
       vault_password_file=$OPTARG
@@ -18,10 +19,13 @@ while getopts ":v:l:t:" opt; do
       tags=$OPTARG
       extra_args="$extra_args -t $tags"
       ;;
+    p )
+      playbook=$OPTARG
+      ;;
     \? )
       echo "Invalid option: $OPTARG" 1>&2
       ;;
   esac
 done
 
-ansible-playbook -v --vault-password-file $vault_password_file -i inventory/bootstrap.yml bastion.yml $extra_args
+ansible-playbook -v --vault-password-file $vault_password_file -i inventory/bootstrap.yml $playbook.yml $extra_args
